e8e5a6f65c0b3beb6e65989cf8e06c4c70338fcd
Sansii <sanbuddhacharyas>
2019-01-19 10:20:03 +0545

    Code with accurate distance calculation and full wide screen

--- stero_vision.py
+++ stero_vision.py
@@ -7,14 +7,18 @@ options ={
     'model' : 'cfg/yolo.cfg',
     'load' : 'bin/yolo.weights',
     'threshold': 0.5,
-    'gpu' : 0.6
+    'gpu' : 0.7
     }
 
 
 tfnet = TFNet(options)
 
-capL = cv2.VideoCapture(2)
+capL = cv2.VideoCapture(0)
+capL.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+capL.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 capR = cv2.VideoCapture(1)
+capR.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+capR.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 XL=0
 XR=0
 distance =0
@@ -25,7 +29,7 @@ while True:
 
     if (retL == True) and (retR == True):
         resultL = tfnet.return_predict(frameL)
-        #resultR = tfnet.return_predict(frameR)
+        resultR = tfnet.return_predict(frameR)
 
         if not resultL == []:
             for res in resultL:
@@ -36,24 +40,9 @@ while True:
 
                 frameL = cv2.rectangle(frameL, tl, br, (255, 0 , 0), 5)
                 frameL = cv2.putText(frameL, label, tl, cv2.FONT_HERSHEY_DUPLEX,1, (0,0,0),2)
-
                 if label == 'bottle':
                     XL = res['topleft']['x']
-                    print("XL:"+str(tl)+'\t' + str(br))
-                    #1st y and then
-    
-                    croped_imageL = frameL[res['topleft']['y']:res['bottomright']['y'],res['topleft']['x']:res['bottomright']['x'],:] 
-                    croped_imageL = cv2.cvtColor(croped_imageL,cv2.COLOR_RGB2GRAY)  
-                    gray_scale_R  = cv2.cvtColor(frameR, cv2.COLOR_RGB2GRAY)
-                    print(frameL.shape)
-                    """
-                    while mean_pre :
-                        croped_imageR = gray_scale_R[res['topleft']['y']:res['bottomright']['y'],res['topleft']['x']:res['bottomright']['x'],:]
-                        subtracted_image = gray_scale_R - croped_imageR
-                    """
-
-
-        """
+                    print("XL:"+str(XL)+'\t')
         if not resultR == []:
             for res in resultR:
         
@@ -66,23 +55,22 @@ while True:
                 if label == 'bottle':
                     XR = res['topleft']['x']
                     print("XR:"+str(XR)+('\n'))
-        
 
-        
+                
     disparity = abs(XL-XR)
     x = frameL.shape[0]
     print("x:"+str(x)+"dis:"+str(disparity))
 
     if not disparity == 0:
         distance = (13*x)/(math.tan(math.radians(30))*disparity)
+        distance = distance -4
 
         
 
     print("distance"+str(distance))
-    """
+    
     cv2.imshow('Left_camera',frameL)
     cv2.imshow('Righr_camera',frameR)
-    cv2.imshow('croped',croped_imageL)
        
     K = cv2.waitKey(10)
     if K == 27:
